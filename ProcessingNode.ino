01ddc423340b45e65f5ac93450dbedb982729bda
Alexander Dada <alexdada555@gmail.com>
2019-03-17 03:51:14 +0000

    Add files via upload

--- ProcessingNode.ino
+++ ProcessingNode.ino
@@ -13,58 +13,86 @@ int Xdir = 7;
 int Xstep = 6;
 int Ydir = 4;
 int Ystep = 5;
+int Xen=8;  //Motor X enable pin
+int Yen=12;  //Motor Y enable pin
 
-int x = 1;
-int y = 1;
+int button = 2;
+int xcount = 0;
+int ycount = 0;
 void setup()
 {
+  Serial.begin(9600);
   pinMode(Xdir,OUTPUT);
   pinMode(Xstep,OUTPUT);
   pinMode(Ydir,OUTPUT);
   pinMode(Ystep,OUTPUT);
-}
+  pinMode(Xen,OUTPUT);
+  pinMode(Yen,OUTPUT);
+  digitalWrite(Xen,LOW);// Low Level Enable
+  digitalWrite(Yen,LOW);// Low Level Enable
 
+  pinMode(button,INPUT);
+}
+// Y limit = 1900 counts
+// X limit = 2320 counts
 void loop()
 {
-   MoveX(5000,1);
-   delay(100);
-   x = 1;
-   MoveX(5000,0);
-   delay(100);
-   x = 1;
+    xMove(1,700);
+    delay(200);
+    yMove(1,700);
+    delay(200);
+    xMove(0,700);
+    delay(200);
+    yMove(0,700);
+    delay(200);
+    while(1){}
 }
 
-void MoveX(int dist ,bool dir)
+int xDir(bool dir)
 {
-  while(x!=0)
-  {
-    delayMicroseconds(2);
-    digitalWrite(Xdir,dir);
-    for(int i = 0; i < dist; i++)
+  digitalWrite(Xdir,dir);
+}
+
+int yDir(bool dir)
+{
+  digitalWrite(Ydir,dir);
+}
+
+int xMove(bool dir,int steps)
+{
+    xcount = 0;
+    digitalWrite(Xen,LOW);// Low Level Enable
+    digitalWrite(Yen,HIGH);// Low Level Disable
+    xDir(dir);
+    yDir(0);
+    while(xcount < steps)
     {
       digitalWrite(Xstep,LOW);
-      delayMicroseconds(2);
+      delayMicroseconds(10);
       digitalWrite(Xstep,HIGH);
       delay(1);
+      xcount++;
     }
-    x = 0;
-  }
+    digitalWrite(Xen,HIGH);// Low Level Enable
+    digitalWrite(Yen,HIGH);// Low Level Enable
 }
 
-void MoveY(int dist ,bool dir)
+int yMove(bool dir,int steps)
 {
-  while(y!=0)
-  {
-    delayMicroseconds(2);
-    digitalWrite(Ydir,dir);
-    for(int j = 0; j < dist; j++)
+    ycount = 0;
+    digitalWrite(Yen,LOW);// Low Level Enable
+    digitalWrite(Xen,HIGH);// Low Level Disable
+    yDir(dir);
+    xDir(0);
+    while(ycount < steps)
     {
       digitalWrite(Ystep,LOW);
-      delayMicroseconds(2);
+      delayMicroseconds(10);
       digitalWrite(Ystep,HIGH);
       delay(1);
+      ycount++;
     }
-    y = 0;
-  }
+    digitalWrite(Xen,HIGH);// Low Level Enable
+    digitalWrite(Yen,HIGH);// Low Level Enable
 }
 
